6b0ae619af39cb8262198ed4492d5e668651b280
mgarcas <miggarcia90@gmail.com>
2023-05-08 21:16:55 +0200

    ghost menus, background picture, etc

--- migkat_wedding.py
+++ migkat_wedding.py
@@ -124,6 +124,6 @@ def test():
 
 
 if __name__ == '__main__':
-    app.run(debug=True) # online
+    # app.run(debug=True) # online
     # app.run(host='192.168.1.59',  debug=True) # Binghamton
-    # app.run(host='192.168.0.7',  debug=True)  # Providence
+    app.run(host='192.168.0.7',  debug=True)  # Providence
