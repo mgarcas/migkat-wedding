2afa41bf761f26b8c378dcfdcaf3b6e6fa6916be
mgarcas <miggarcia90@gmail.com>
2023-05-24 05:36:45 +0200

    change color header, quote Corinthians shortened

--- models/user.py
+++ models/user.py
@@ -6,10 +6,8 @@ class User (UserMixin):
         self.num = num
         self.username = user_id
         self.password = password
-        print('######## init ########')
 
     def is_authenticated(self):
-        print('######## authenticator ########')
         # Check if the user is authenticated
         # Return True if authenticated, False otherwise
         return True
@@ -17,16 +15,13 @@ class User (UserMixin):
     def is_active(self):
         # Check if the user is active
         # Return True if active, False otherwise
-        print('######## is_active ########')
         return True
 
     def is_anonymous(self):
         # Check if the user is anonymous
         # Return False since we have real users
-        print('######## is_anonymous ########')
         return False
 
     def get_id(self):
         # Return the user's ID as a string
-        print('######## get_id ########')
         return str(self.username)
